2636a96a6cdc5aae359cbf2abbda35eb3203f934
kakha77 <kaxuna.kalandadze@gmail.com>
2025-01-04 15:49:05 +0400

    first commit

---
+++ Day11/homework/GOA.py
@@ -0,0 +1,52 @@
+x = 10
+y = 20
+print(x > y)  # False
+print(x < y)  # True
+print(x >= y)  # False
+print(x <= y)  # True
+print(x != y)  # True
+print(x == y)  # False
+
+
+
+a = 15
+b = 10
+print(a > b)  # True
+print(a < b)  # False
+print(a >= b)  # True
+print(a <= b)  # False
+print(a != b)  # True
+print(a == b)  # False
+
+
+
+x = 5
+y = 5
+print(x > y)  # False
+print(x < y)  # False
+print(x >= y)  # True
+print(x <= y)  # True
+print(x != y)  # False
+print(x == y)  # True
+
+
+
+x = -3
+y = -5
+print(x > y)  # True
+print(x < y)  # False
+print(x >= y)  # True
+print(x <= y)  # False
+print(x != y)  # True
+print(x == y)  # False
+
+
+
+x = 0
+y = 0
+print(x > y)  # False
+print(x < y)  # False
+print(x >= y)  # True
+print(x <= y)  # True
+print(x != y)  # False
+print(x == y)  # True
---
+++ Day11/homework/index.py
@@ -0,0 +1,6 @@
+
+age = int(input("შეიყვანეთ თქვენი ასაკი: "))
+
+
+future_age = age + 20
+print("თქვენ 20 წელში იქნებით "+str(future_age))
