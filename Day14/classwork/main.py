f1e78c3bd69d9e67fd5665cc6be26390b44ecfd9
kakha77 <kaxuna.kalandadze@gmail.com>
2025-02-02 14:38:00 +0400

    commit

---
+++ Day14/classwork/main.py
@@ -0,0 +1,10 @@
+print(10<15 or 7<8)#True
+print(10<15 or 7>8)#True
+print(10>15 or 7<8)#True
+print(10>15 or 7>8)#False
+
+
+print(10>15 and 7<8)#False
+print(10<15 and 7>8)#False
+print(10>15 and 7>8)#False
+print(10<15 and 7<8)#True
