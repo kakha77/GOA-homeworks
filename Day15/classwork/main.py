50e55f56f5747ccc0a035eafb5858102de624397
kakha77 <kaxuna.kalandadze@gmail.com>
2025-02-08 12:55:20 +0400

    commit

---
+++ Day15/classwork/main.py
@@ -0,0 +1,17 @@
+for i in range(950, 1001, 2):
+    print(i)
+
+for num in range(100):
+    print('kakha')  
+
+for x in range(1000):
+    print('Goa Is The Best')
+
+for i in range(100, 200):
+    print(i)
+
+for i in range(200, 250, 15):
+    print(i)
+
+for x in range(200, 400):
+    print(x)
---
+++ Day15/homework/index.py
@@ -0,0 +1,19 @@
+for i in range(300, 451, 10):
+    print(i)
+
+
+for i in range(200, 500, 2):
+   print(i)
+
+for i in range(0, 50, 3):
+    print(i)
+
+
+for i in range(1000, 1101, 2):
+    print(i)
+
+for i in range(1, 10):
+    print(i)
+
+for i in range(2000, 2021):
+    print(i)
