7842a75c93565af82418ba7c24c8cdd8b6d97062
kakha77 <kaxuna.kalandadze@gmail.com>
2025-02-22 18:34:36 +0400

    commit

---
+++ Day16/classwork/main.py
@@ -0,0 +1,5 @@
+for i in range (150, 203, 2):
+    print(i)
+
+for x in range (400, 199, -1):
+    print(x)
---
+++ Day16/homework/main.py
@@ -0,0 +1,21 @@
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+x = 1
+while x <= 50:
+    print(x)
+    x += 1
+
+
+names = ["ნიკა", "თეო", "ანა", "გიო"]
+count = 0
+for name in names:
+    count += 1
+print(count)
+
+
+for _ in range(7):
+    print("მე მიყვარს პროგრამირება")
