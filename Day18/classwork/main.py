f88c570b306e996abd3c3ca7b7e6ae7977e97b87
kakha77 <kaxuna.kalandadze@gmail.com>
2025-03-08 15:08:45 +0400

    commit

---
+++ Day18/classwork/main.py
@@ -0,0 +1,10 @@
+car=["BMW", "mercedes", "opel", "AUDI", "tesla", "lamborghini", "Bugatti", "porche", "MClaren", "sedan"]
+print(car)
+
+favfood=["shawarma","burger","apple","banana","chikhirtma"]
+print(favfood)
+
+num = [1,  2,  3,   4,  5,  6,  7,   8,  9,  10]
+print(num)
+print(num[1:8])
+print(num[4:10])
