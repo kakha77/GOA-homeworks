2521781397f3ca75e35f7e7ccfd221bb3390caa9
kakha77 <kaxuna.kalandadze@gmail.com>
2025-03-22 21:11:45 +0400

    commit

---
+++ Day20/classwork/main.py
@@ -0,0 +1,24 @@
+num1 = [1, 2, 3, 4, 5]
+    #   0  1  2  3  4
+print(num1[0])
+print(num1[4])
+print(num1[2])
+
+
+num = [1, 3, 3, 4, 5, 6, 6, 8, 9, 12]
+    #  0  1  2  3  4  5  6  7  8   9
+num[1] = 2
+num[6] = 7
+num[9] = 10
+print(num)
+
+
+h1 = "hellowen"
+print(h1[0] + h1[1] + h1[2] + h1[3] + h1[4]) 
+print(h1[0] + h1[1] + h1[2] + h1[3])
+print(h1[5] + h1[1] + h1[2] + h1[3])
+print(h1[5] + h1[4] + h1[7])
+
+
+GOA1 = ["GOA" , "MAGARIA", "NOVATORI","IS","THE", "VIRGIN", "CHAD" ,"BEST" ,"ACADEMY"]
+print(GOA1[0], GOA1[3], GOA1[4], GOA1[7], GOA1[8])
