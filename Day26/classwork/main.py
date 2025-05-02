c1865f572e424c0016439317f7b0789733de7eed
kakha77 <kaxuna.kalandadze@gmail.com>
2025-05-02 14:12:58 +0400

    commit

--- Day26/classwork/main.py
+++ Day26/classwork/main.py
@@ -1,16 +1,1 @@
-def name_to_upper(name):
-    result = ""
-    for letter in name:
-        result += letter.upper()
-    return result
-
-print(name_to_upper("kakha"))
-
-
-
-
-def elements(my_list):
-    for element in my_list:
-        print(element)
-
-elements(["apple", "banana", "mango"])
+#საკლასო არ გვქონდა
--- Day28/homework/main.py
+++ Day28/homework/main.py
@@ -1,1 +1,25 @@
-#საშინაო არ გვქონდა
+def sum_odd_numbers(numbers):
+    return sum(num for num in numbers if num % 2 != 0)
+print(sum_odd_numbers([1, 2, 3, 4, 5]))
+
+
+
+
+def four_letter_names(names):
+    return [name for name in names if len(name) == 4]
+print(four_letter_names(["Anna", "Giorgi", "Luka", "Nika"]))
+
+
+
+
+def divisible_by_3_and_5(numbers):
+    return [num for num in numbers if num % 3 == 0 and num % 5 == 0]
+print(divisible_by_3_and_5([15, 30, 10, 9, 5]))
+
+
+
+
+
+name = "Kakha"
+age = 13
+print(f"My name is {name} and I am {age} years old.")
