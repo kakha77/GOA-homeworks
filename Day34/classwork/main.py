b184df77e068dd01453c771fa06d087bb9e69763
kakha77 <kaxuna.kalandadze@gmail.com>
2025-06-21 14:50:17 +0400

    commit

---
+++ Day34/classwork/main.py
@@ -0,0 +1,19 @@
+def greet(a, b):
+    print(a + b)
+greet(5, 7)
+
+
+
+def greet1(name):
+    print("hello,", name)
+greet1("kakha")
+
+
+
+def greet2(a, b, c, d, e):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+greet2(1, 2, 3, 4, 5)
