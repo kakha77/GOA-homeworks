1efae3fecc3c8725ce0712c95ab88cce839f3d61
kakha77 <kaxuna.kalandadze@gmail.com>
2025-07-06 20:27:06 +0400

    commit

---
+++ Day36/homework/codewars.py
@@ -0,0 +1,20 @@
+def invert(lst):
+    return [-x for x in lst]
+
+
+def monkey_count(n):
+    return list(range(1, n+1))
+
+
+def remove_exclamation_marks(s):
+    return s.replace("!", "")
+
+
+
+def reverse_words(s):
+    return " ".join(s.split()[::-1])
+
+
+
+def string_to_array(s):
+    return s.split(" ")
