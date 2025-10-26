4213190cecb7c5f3a028689b93678969c3f85399
kakha77 <kaxuna.kalandadze@gmail.com>
2025-10-26 10:30:01 +0400

    commit

--- Day51/homework/py.py
+++ Day51/homework/py.py
@@ -1,18 +1,16 @@
 def calculator(x, y, op):
-    if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+    if type(x)not in[int,float] or type(y)not in[int,float]:
         return "unknown value"
-
-    match op:
-        case "+":
-            return x + y
-        case "-":
-            return x - y
-        case "*":
-            return x * y
-        case "/":
-            return x / y if y != 0 else "unknown value"
-        case _: 
-            return "unknown value"
+    if op == "+":
+        return x + y
+    elif op == "-":
+        return x - y
+    elif op == "*":
+        return x * y
+    elif op == "/":
+        return x / y
+    else:
+        return "unknown value"
         
 
 
